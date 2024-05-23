658902378130ac5054fb2c525794fa6223eb5ea5
luciano guedes da silva <luciano.guuedessilva@gmail.com>
2024-05-23 20:14:18 -0300

    java7
    java

---
+++ aula7.js
@@ -0,0 +1,22 @@
+let num1 =133;
+let num2 =45;
+
+console.log(num1 / num2);
+
+const result =num1 + num2;
+console.log(result);
+
+const sub = num1 - num2;
+console.log(sub);
+
+const div =num1 / num2;
+console.log(div);
+
+const mult =num1 * num2;
+console.log(mult);
+
+num1 =33;
+num2 =222;
+
+console.log(num1 >num2);
+console.log(num2 > num1);
