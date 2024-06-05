881ffd821f7bffd6b4f7872803ae4ec8df11d1b0
luciano guedes da silva <luciano.guuedessilva@gmail.com>
2024-06-04 21:42:34 -0300

    javascripit
    java

---
+++ aula8.js
@@ -0,0 +1,14 @@
+const texto1 ='aqui temos uma string normal'
+
+
+const texto2 ="já esta , apesar das aspas duplas ao invés das simples, também é uma string";
+
+
+const numeroRegistro = 31472;
+
+
+const stringTemplate =`O número que quero registrar é o ${numeroRegistro}`;
+
+
+console.log(stringTemplate);
+
---
+++ aula9.js
@@ -0,0 +1,10 @@
+const grupoNunber = ['azul','amarelo','vermelho','preto','branco'];
+
+console.log(grupoNunber);
+
+
+console.log(`O valor do  elemento 1 da minha lista é ${grupoNunber [0] }`);
+console.log(`O valor do  elemento 1 da minha lista é ${grupoNunber [1] }`);
+console.log(`O valor do  elemento 1 da minha lista é ${grupoNunber [2]}`);
+console.log(`O valor do  elemento 1 da minha lista é ${grupoNunber [3] }`);
+console.log(`O valor do  elemento 1 da minha lista é ${grupoNunber [4] }`);
